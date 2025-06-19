1751cae539dffbfcc3d99187239acfcb87948024
AnkitaJain-25 <ankita.jain25@outlook.com>
2025-06-19 15:45:53 +0530

    Vite + React + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="text-3xl font-bold text-green-800">
+      <h1>Welcome to My React App</h1>
+      <p>Let's build Netflix GPT</p>
+    </div>
+  )
+}
+
+export default App
